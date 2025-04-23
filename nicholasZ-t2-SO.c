835f924fc738c71afe941adc411ac377b8664787
Nicholas Zortea Graczik <zorteanicholas619@gmail.com>
2025-04-23 19:03:53 -0300

    Fix: Refatorado

--- nicholasZ-t2-SO.c
+++ nicholasZ-t2-SO.c
@@ -5,48 +5,56 @@
 #include <time.h>
 
 void createTreeOfProcess(int height);
+
 double elapsedTime(struct timespec start, struct timespec end);
 
+void testTree(pid_t originalPid,int treeHeight);
+
 int main(int argc, char *argv[]){
-    int treeHeight = atoi(argv[1]);
+    int userInput = atoi(argv[1]);
     pid_t originalPid = getpid();
+    
+    testTree(originalPid, userInput);
+    
+    return 0;
+}
+
+void testTree(pid_t originalPid, int treeHeight){
     struct timespec start, end;
     
     clock_gettime(CLOCK_MONOTONIC, &start);
     createTreeOfProcess(treeHeight);
     clock_gettime(CLOCK_MONOTONIC, &end);
-    if(getpid() == originalPid){
-        printf("Tempo demorado criando árvore: %.6f segundos\n", elapsedTime(start,end));
-    }
-    
-    return 0;
+    printf("Tempo demorado criando árvore: %.6f segundos\n", elapsedTime(start,end));
 }
 
 double elapsedTime(struct timespec start, struct timespec end){
     return (end.tv_sec - start.tv_sec) + 
-    (end.tv_nsec - start.tv_nsec) / 1e9;
+    ((end.tv_nsec - start.tv_nsec) / 1e9);
 }
 
 void createTreeOfProcess(int height){
     if(height == 0) return;
-    
-    int status;
-    pid_t pid;
-    pid = fork();
-    
-    if(pid == 0){// eh um filho deve criar dois filhos
-        printf("sou filho, meu pid eh: %d, meu pai eh: %d \n", getpid(), getppid());
-        createTreeOfProcess(height-1);
+
+    pid_t c1 = fork();
+    if (c1 == 0) {
+        // Filho 1
+        printf("sou filho (1), pid: %d, pai: %d\n", getpid(), getppid());
+        createTreeOfProcess(height - 1);
         exit(0);
-    } else if(pid > 0){//eh o pai deve criar outro filho
-        printf("sou o pai, meu pid eh: %d, meu pai eh: %d \n", getpid(), getppid());
-        pid_t outroPid = fork();
-        if(outroPid == 0){ //eh o outro filho, deve criar dois filhos
-            printf("sou filho, meu pid eh: %d, meu pai eh: %d \n", getpid(), getppid());
-            createTreeOfProcess(height-1);
+    } else {
+        printf("sou o pai, pid: %d, meu pai eh: %d\n", getpid(), getppid());
+        pid_t c2 = fork();
+        if (c2 == 0) {
+            // Filho 2
+            printf("sou filho (2), pid: %d, pai: %d\n", getpid(), getppid());
+            createTreeOfProcess(height - 1);
             exit(0);
-        } else if(pid > 0){//eh o pai deve esperar
-            wait(&status);
+        } else {
+            // Só o pai chega aqui
+            waitpid(c1, NULL, 0);
+            waitpid(c2, NULL, 0);
         }
     }
 }
+
